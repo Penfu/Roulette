2d8ce79270d01fbe5f1a85354b2b84d829f36b38
Penfu <sypenfu@gmail.com>
2022-07-16 11:21:46 +0200

    Init VueJS

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+
+import './assets/main.css'
+
+const app = createApp(App)
+
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+	broadcaster: 'pusher',
+	key: import.meta.env.VITE_APP_WEBSOCKET_KEY,
+	wsHost: import.meta.env.VITE_APP_WEBSOCKET_SERVER,
+	wsPort: 6001,
+	forceTLS: false,
+	disableStats: true,
+});
+
+app.use(router)
+
+app.mount('#app')
