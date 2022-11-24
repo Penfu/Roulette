8d4bcc5185d6d72131a95251b5c5389678fc4e64
Armand Marechal <sypenfu@gmail.com>
2022-11-24 17:29:05 +0100

    Refactor templating

--- src/components/Bets.vue
+++ src/components/Bets.vue
@@ -17,8 +17,8 @@ export default {
       type: Number,
       required: true,
     },
-    bets : {
-      type: Array,
+    bets: {
+      type: Array as () => Array<{ user: string, color: string; value: number }>,
       required: true,
     },
   },
@@ -40,17 +40,12 @@ export default {
 
 <template>
   <div class="basis-1/3 flex flex-col space-y-4">
-    <button
-      :disable="() => !active"
-      @click="addBet"
-      class="py-4 font-semibold rounded shadow"
-      :class="{
-        'bg-red-500 hover:bg-red-600 shadow-red-300': color == Color.RED,
-        'bg-green-500 hover:bg-green-600 shadow-green-300': color == Color.GREEN,
-        'bg-gray-900 hover:bg-black shadow-gray-500': color == Color.BLACK,
-      }"
-    >
-      x {{ value}}
+    <button :disable="() => !active" @click="addBet" class="py-4 font-semibold rounded shadow" :class="{
+      'bg-red-500 hover:bg-red-600 shadow-red-300': color == Color.RED,
+      'bg-green-500 hover:bg-green-600 shadow-green-300': color == Color.GREEN,
+      'bg-gray-900 hover:bg-black shadow-gray-500': color == Color.BLACK,
+    }">
+      x {{ value }}
     </button>
     <div
       class="p-2 h-64 overflow-y-hidden space-y-2 bg-gray-100 text-left text-xl text-gray-800 rounded shadow shadow-gray-300">
--- src/components/Histories.vue
+++ src/components/Histories.vue
@@ -15,11 +15,11 @@ export default {
 </script>
 
 <template>
-    <div v-if="histories.length == 0" class="p-2 grid grid-flow-col grid-rows-2 lg:grid-rows-1 gap-2">
-      <div v-for="x in 10" :key="x" class="flex justify-center items-center text-center h-12 w-12 bg-gray-800 rounded shadow animate-pulse"></div>
-    </div>
-    <div v-else class="p-2 grid grid-flow-col grid-rows-2 lg:grid-rows-1 gap-2">
-      <History v-for="(history, index) in histories" :key="index" :color="history.color"
-        :value="history.value" />
-    </div>
+  <div v-if="histories.length == 0" class="p-2 grid grid-flow-col grid-rows-2 lg:grid-rows-1 gap-2">
+    <div v-for="x in 10" :key="x"
+      class="flex justify-center items-center text-center h-12 w-12 bg-gray-800 rounded shadow animate-pulse"></div>
+  </div>
+  <div v-else class="p-2 grid grid-flow-col grid-rows-2 lg:grid-rows-1 gap-2">
+    <History v-for="(history, index) in histories" :key="index" :color="history.color" :value="history.value" />
+  </div>
 </template>
