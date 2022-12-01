e80b906d6a482f911cc7b3686ab114bf84a604be
Armand Marechal <sypenfu@gmail.com>
2022-12-01 09:56:49 +0100

    Add color bets animation

--- src/components/Bets.vue
+++ src/components/Bets.vue
@@ -26,6 +26,7 @@ export default {
     return {
       Color,
       bet: Bet,
+      hovered: false,
     };
   },
   methods: {
@@ -40,16 +41,32 @@ export default {
 
 <template>
   <div class="basis-1/3 flex flex-col space-y-4">
-    <button :disable="() => !active" @click="addBet" class="py-4 font-semibold rounded shadow" :class="{
-      'bg-red-500 hover:bg-red-600 shadow-red-300': color == Color.RED,
-      'bg-green-500 hover:bg-green-600 shadow-green-300': color == Color.GREEN,
-      'bg-gray-900 hover:bg-black shadow-gray-500': color == Color.BLACK,
-    }">
+    <button
+      :disable="() => !active"
+      @click="addBet"
+      @mouseover="hovered = true"
+      @mouseout="hovered = false"
+      class="py-4 font-semibold rounded shadow transition-transform duration-300 ease-in-out"
+      :class="{
+        'hover:scale-105': active,
+        'bg-red-500 hover:bg-red-600 shadow-red-300': color == Color.RED,
+        'bg-green-500 hover:bg-green-600 shadow-green-300':
+          color == Color.GREEN,
+        'bg-gray-900 hover:bg-black shadow-gray-500': color == Color.BLACK,
+      }"
+    >
       x {{ value }}
     </button>
     <div
-      class="p-2 h-64 overflow-y-hidden space-y-2 bg-white text-left text-xl text-gray-800 rounded shadow shadow-gray-300">
-      <Bet v-for="(bet, index) in bets" :key="index" :name="bet.user" :value="bet.value" />
+      class="p-2 h-64 overflow-y-hidden space-y-2 bg-white text-left text-xl text-gray-800 rounded shadow shadow-gray-300 transition-transform duration-300 ease-in-out"
+      :class="{ 'scale-105': active && hovered }"
+    >
+      <Bet
+        v-for="(bet, index) in bets"
+        :key="index"
+        :name="bet.user"
+        :value="bet.value"
+      />
     </div>
   </div>
 </template>
