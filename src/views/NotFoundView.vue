dd281f3ebafc8eae29895a5c22c4d9830b3f5b60
Armand Marechal <sypenfu@gmail.com>
2023-01-22 14:51:46 +0100

    Update 404

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,6 +1,8 @@
 <template>
-  <main class="">
-    <h4 class="text-xl">Page not found</h4>
-    <h2 class="text-9xl font-bold">404</h2>
+  <main class="flex justify-center items-center">
+    <div class="text-center animate-bounce duration-1000">
+      <h2 class="text-9xl font-bold">404</h2>
+      <h4 class="text-3xl">Page not found</h4>
+    </div>
   </main>
 </template>
