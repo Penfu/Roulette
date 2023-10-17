7b50de686c577ef4192d60390fcd0202a75284dc
Armand Marechal <sypenfu@gmail.com>
2023-10-17 12:21:29 +0200

    Add animation on card opening

--- src/components/user/activity/bet/BetActivity.vue
+++ src/components/user/activity/bet/BetActivity.vue
@@ -11,24 +11,29 @@ defineProps<{
   bet: Bet;
 }>();
 
-const RollActivity = defineAsyncComponent(() => import("@/components/user/activity/roll/RollActivity.vue"));
+const RollActivity = defineAsyncComponent(
+  () => import("@/components/user/activity/roll/RollActivity.vue")
+);
 
 const isOpen = ref(false);
 </script>
 
 <template>
-  <div class="flex flex-col bg-gray-50 rounded-lg border border-gray-300">
+  <div class="flex flex-col bg-gray-50 rounded-lg border border-gray-300 overflow-hidden">
     <div @click="isOpen = !isOpen" class="p-6 flex items-center space-x-8 hover:cursor-pointer">
       <div class="grow flex items-center space-x-4">
         <div class="w-8 h-8 rounded shadow-md" :class="getClassFromColor(bet.color)" />
         <span>{{ bet.amount }}</span>
       </div>
 
-      <span class="w-14 uppercase text-gray-800 font-bold rounded shadow-md" :class="{
-        'bg-green-300  shadow-green-400': bet.isWin,
-        'bg-red-300  shadow-red-500': !bet.isWin
-      }">
-        {{ bet.isWin ? 'Win' : 'Lose' }}
+      <span
+        class="w-14 uppercase text-gray-800 font-bold rounded shadow-md"
+        :class="{
+          'bg-green-300  shadow-green-400': bet.isWin,
+          'bg-red-300  shadow-red-500': !bet.isWin,
+        }"
+      >
+        {{ bet.isWin ? "Win" : "Lose" }}
       </span>
 
       <span class="px-6">{{ bet.formattedCreatedAt }}</span>
@@ -38,13 +43,20 @@ const isOpen = ref(false);
       </button>
     </div>
 
-    <Suspense v-if="isOpen">
-      <template #default>
-        <RollActivity :bet="bet" />
-      </template>
-      <template #fallback>
-        <RollActivityLoading />
-      </template>
-    </Suspense>
+    <div
+      v-motion
+      :initial="{ opacity: 0, y: -100 }"
+      :enter="{ opacity: 1, y: 0, transition: { type: 'keyframes', ease: 'easeInOut', duration: 150 } }"
+      v-if="isOpen"
+    >
+      <Suspense>
+        <template #default>
+          <RollActivity :bet="bet" />
+        </template>
+        <template #fallback>
+          <RollActivityLoading />
+        </template>
+      </Suspense>
+    </div>
   </div>
 </template>
