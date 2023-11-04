6478f5e366145be3b0f54955d1b34c8d035a4b49
Penfu <sypenfu@gmail.com>
2023-11-04 18:25:17 +0100

    Ajust modal responsive

--- src/components/settings/account/DeleteAccount.vue
+++ src/components/settings/account/DeleteAccount.vue
@@ -70,59 +70,59 @@ const handleConfirm = async () => {
         leave="duration-200 ease-in"
         leave-from="opacity-100"
         leave-to="opacity-0"
+        as="template"
       >
         <div class="fixed inset-0 bg-black/75" />
       </TransitionChild>
 
-      <div class="fixed inset-0">
-        <div class="min-h-full flex justify-center items-end sm:items-center">
-          <TransitionChild
-            enter="duration-300 ease-out"
-            enter-from="translate-y-12 sm:opacity-0 sm:translate-y-0 sm:scale-95"
-            enter-to="opacity-100 translate-y-0 sm:scale-100"
-            leave="duration-200 ease-in"
-            leave-from="translate-y-0 sm:opacity-100 sm:scale-100"
-            leave-to="translate-y-12 sm:opacity-0 sm:translate-y-0 sm:scale-95"
+      <div class="fixed inset-0 flex justify-center items-end sm:items-center">
+        <TransitionChild
+          enter="duration-300 ease-out"
+          enter-from="translate-y-12 sm:opacity-0 sm:translate-y-0 sm:scale-95"
+          enter-to="opacity-100 translate-y-0 sm:scale-100"
+          leave="duration-200 ease-in"
+          leave-from="translate-y-0 sm:opacity-100 sm:scale-100"
+          leave-to="translate-y-12 sm:opacity-0 sm:translate-y-0 sm:scale-95"
+          as="template"
+        >
+          <DialogPanel
+            class="sm:mx-8 px-6 py-8 md:px-8 md:py-10 max-w-2xl w-full bg-bkg-1 rounded-t-3xl sm:rounded-lg space-y-12"
           >
-            <DialogPanel
-              class="sm:mx-8 px-6 py-8 md:px-8 md:py-10 max-w-2xl w-full bg-bkg-1 rounded-t-3xl sm:rounded-lg space-y-12"
-            >
-              <DialogTitle as="h2" class="text-3xl font-bold">
-                You're about to delete your account
-              </DialogTitle>
+            <DialogTitle as="h2" class="text-3xl font-bold">
+              You're about to delete your account
+            </DialogTitle>
 
-              <div class="space-y-8">
-                <p v-if="error" class="text-red">{{ error }}</p>
-                <p class="text-gray-700">
-                  This is <span class="text-red font-semibold">irreversible</span>. All your data
-                  will be deleted including your balance, bets, and profile.
-                </p>
-                <div class="space-y-2">
-                  <label for="keyword" class="font-semibold space-x-2">
-                    <span>Enter your name</span>
-                    <span class="px-2 py-1 text-green-dark bg-green-100 rounded-md">
-                      {{ user.name }}
-                    </span>
-                    <span>to continue:</span>
-                  </label>
+            <div class="space-y-8">
+              <p v-if="error" class="text-red">{{ error }}</p>
+              <p class="text-gray-700">
+                This is <span class="text-red font-semibold">irreversible</span>. All your data will
+                be deleted including your balance, bets, and profile.
+              </p>
+              <div class="space-y-2">
+                <label for="keyword" class="font-semibold space-x-2">
+                  <span>Enter your name</span>
+                  <span class="px-2 py-1 text-green-dark bg-green-100 rounded-md">
+                    {{ user.name }}
+                  </span>
+                  <span>to continue:</span>
+                </label>
 
-                  <input v-model="keyword" id="keyword" type="text" />
-                </div>
+                <input v-model="keyword" id="keyword" type="text" />
               </div>
+            </div>
 
-              <div class="flex flex-col-reverse md:flex-row gap-2 mt-4">
-                <button @click="closeModal" class="btn-secondary w-full">Cancel</button>
-                <button
-                  @click="handleConfirm"
-                  :disabled="keyword !== user.name"
-                  class="btn-danger w-full"
-                >
-                  Delete account
-                </button>
-              </div>
-            </DialogPanel>
-          </TransitionChild>
-        </div>
+            <div class="flex flex-col-reverse md:flex-row gap-2 mt-4">
+              <button @click="closeModal" class="btn-secondary w-full">Cancel</button>
+              <button
+                @click="handleConfirm"
+                :disabled="keyword !== user.name"
+                class="btn-danger w-full"
+              >
+                Delete account
+              </button>
+            </div>
+          </DialogPanel>
+        </TransitionChild>
       </div>
     </Dialog>
   </TransitionRoot>
