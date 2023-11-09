f07cbc16abaf2b8be6bd2d1037e34c8ac37acf14
Penfu <sypenfu@gmail.com>
2023-11-09 13:31:13 +0100

    Refactor wheel angle animation

--- src/components/game/rolls/RouletteSection.vue
+++ src/components/game/rolls/RouletteSection.vue
@@ -1,64 +1,53 @@
 <script setup lang="ts">
 import { ref, watch } from "vue";
+import { storeToRefs } from "pinia";
 import anime from "animejs";
 
 import { useGameStore } from "@/stores/game";
 
 import { RollStep } from "@/enums/step";
-import { Color } from "@/enums/color";
 import { classFromColor } from "@/helpers/color";
 
-import ChevronDownIcon from "@/components/icons/ChevronDownIcon.vue";
 import HistoryLayout from "@/components/game/rolls/HistoryLayout.vue";
+import ChevronDownIcon from "@/components/icons/ChevronDownIcon.vue";
 
 defineProps<{
   message: string;
 }>();
 
 const game = useGameStore();
+const { step, result } = storeToRefs(game);
 
 const wheel = ref(null);
 
+const caseCount = 11;
+const caseSize = 360 / caseCount;
+const halfCaseSize = caseSize / 2;
+const caseMarge = caseSize / 10;
+
+const minAngle = caseMarge;
+const maxAngle = caseSize - caseMarge;
+
+const minTurn = 3;
+const maxTurn = 5;
+
 watch(
-  () => game.step,
+  () => step.value,
   (step) => {
-    if (step === RollStep.ROLL && game.result) {
+    if (step === RollStep.ROLL && result.value) {
       // Stop the wheel at the winning case
-      const caseSize = 360 / 11; // 11 cases
-      const halfCaseSize = caseSize / 2;
-      const marge = caseSize / 10;
-
       let finalAngle = 0;
+      finalAngle = result.value.value * caseSize - halfCaseSize;
+      finalAngle += Math.random() * (maxAngle - minAngle) + minAngle; // Add a random angle
 
-      if (game.result.color === Color.RED) {
-        const winningCaseIndex = Math.floor(Math.random() * 5); // 5 possible cases for red
-        finalAngle = halfCaseSize; // Add Green case before
-        finalAngle += caseSize + winningCaseIndex * 2 * caseSize; // Add Red(s) and Black(s) cases before
-        finalAngle += anime.random(marge, caseSize - marge);
-      } else if (game.result.color === Color.BLACK) {
-        const winningCaseIndex = Math.floor(Math.random() * 5) + 1; // 5 possible cases for black
-        finalAngle = halfCaseSize; // Add Green case before
-        finalAngle += winningCaseIndex * 2 * caseSize; // Add Red(s) and Black(s) cases before
-        finalAngle += anime.random(marge, caseSize - marge); // Add random size inside the range with marge
-      } else if (game.result.color === Color.GREEN) {
-        finalAngle = 360 + anime.random(-halfCaseSize + marge, halfCaseSize - marge);
-      }
-
-      const currentRotation = parseFloat(anime.get(wheel.value, "rotate") as string);
-      const rotationToEndTheTurn = 360 - (currentRotation % 360);
-
-      finalAngle += currentRotation + rotationToEndTheTurn;
+      const turn = Math.round(Math.random() * (maxTurn - minTurn) + minTurn);
 
       anime({
         targets: wheel.value,
-        rotate: 360 * 3 + finalAngle,
+        rotate: 360 * turn - finalAngle,
         duration: 5500,
         easing: "easeInOutQuad",
         paused: false,
-
-        begin: function () {
-          anime.set(wheel.value, { rotate: currentRotation });
-        },
       });
     } else if (step === RollStep.RESET) {
       anime
@@ -103,18 +92,18 @@ watch(
         <div
           class="h-14 flex grow justify-center items-center text-center text-3xl xl:text-5xl font-semibold uppercase"
         >
-          <span v-show="game.step === RollStep.BET">{{ message }}</span>
-          <span v-show="game.step === RollStep.ROLL"> Rolling... </span>
+          <span v-show="step === RollStep.BET">{{ message }}</span>
+          <span v-show="step === RollStep.ROLL"> Rolling... </span>
           <div
-            v-if="game.step === RollStep.DISPLAY_RESULT && game.result"
+            v-if="step === RollStep.DISPLAY_RESULT && result"
             class="flex items-center justify-center space-x-4"
           >
             <span>Result</span>
             <span
-              class="h-12 w-12 xl:h-20 xl:w-20 flex items-center justify-center text-white rounded text-center shadow"
-              :class="classFromColor(game.result.color)"
+              class="h-12 w-12 xl:h-20 xl:w-20 flex items-center justify-center text-white rounded-lg shadow"
+              :class="classFromColor(result.color)"
             >
-              {{ game.result.value }}
+              {{ result.value }}
             </span>
           </div>
         </div>
